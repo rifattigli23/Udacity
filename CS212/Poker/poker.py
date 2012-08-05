7939553cb1f474de69e2825d0b576511ede01f53
Graham Madden <maddenwg@gmail.com>
2012-08-05 13:34:30 -0400

    Add test cases

--- CS212/Poker/poker.py
+++ CS212/Poker/poker.py
@@ -4,3 +4,17 @@ def poker(hands):
 
 def hand_rank(hand):
   return None # we wil be changing this later
+
+def test():
+  "Test cases for the functions in poker program."
+  sf = "6C 7C 8C 9C TC".split()
+  fk = "9D 9H 9S 9C 7D".split()
+  fh = "TD TC TH 7C 7D".split()
+  assert poker([sf, fk, fh]) == sf
+  assert poker([fk,fh]) == fk
+  assert poker([fh, fh]) == fh
+  assert poker([sf]) == sf
+  assert poker([sh] + 99*[fh]) == sf 
+  return "tests pass"
+
+print test()
