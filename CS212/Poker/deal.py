3c56241e017088eef3109320c51c3546f0abc4ed
Graham Madden <maddenwg@gmail.com>
2012-08-12 12:38:19 -0400

    simplified deal()

--- CS212/Poker/deal.py
+++ CS212/Poker/deal.py
@@ -1,30 +1,6 @@
-# -----------
-# User Instructions
-# 
-# Write a function, deal(numhands, n=5, deck), that 
-# deals numhands hands with n cards each.
-#
+import random 
 
-import random # this will be a useful library for shuffling
-
-# This builds a deck of 52 cards. If you are unfamiliar
-# with this notation, check out Andy's supplemental video
-# on list comprehensions (you can find the link in the 
-# Instructor Comments box below).
-
-mydeck = [r+s for r in '23456789TJQKA' for s in 'SHDC'] 
-
-def deal(numhands, n=5, deck=mydeck):
-    random.shuffle(mydeck)
-
-    hands = []
-    currenthand = []
-
-    while len(hands) < numhands:
-        currenthand.append(mydeck.pop(0))
-        
-        if len(currenthand) == n:
-            hands.append(currenthand)
-            currenthand = []
-            
-    return hands
+def deal(numhands, n=5, deck=[t+s for r in '23456789TJQKA' for s in 'SHDC']):
+  "Shuffle the deck and deal out numhands n-card hands."
+  random.shuffle(deck)
+  return [deck[n*i:n*(i+1)] for i in range(numhands)]
