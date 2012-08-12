a93943d5355a78baa9487c7a76d64c95ce6cc467
Graham Madden <maddenwg@gmail.com>
2012-08-12 13:43:06 -0400

    remove deal.py

--- CS212/Poker/deal.py
+++
@@ -1,6 +0,0 @@
-import random 
-
-def deal(numhands, n=5, deck=[r+s for r in '23456789TJQKA' for s in 'SHDC']):
-  "Shuffle the deck and deal out numhands n-card hands."
-  random.shuffle(deck)
-  return [deck[n*i:n*(i+1)] for i in range(numhands)]
