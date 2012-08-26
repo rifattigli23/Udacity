a02c9cdbbc59712612b9fc86486c19b11214fb44
Graham Madden <maddenwg@gmail.com>
2012-08-26 16:04:41 -0400

    improved speed by filtering out redundant checks

--- CS212/Unit2/zebraPuzzle.py
+++ CS212/Unit2/zebraPuzzle.py
@@ -1,27 +1,39 @@
 import itertools
 
+def imright(h1, h2):
+  return h1-h2 == -1
+
+def nextto(h1, h2):
+  return abs(h1-h2) == 1
+
 def zebra_puzzle():
   "Return a tuple (WATER, ZEBRA) indicating their house numbers."
   houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
-  orderings = list(itertols.permutations(houses)) #1
+  orderings = list(itertools.permutations(houses)) #1
   return next((WATER, ZEBRA)
           for (red, green, ivory, yellow, blue) in orderings
+          
           for (Englishman, Spaniard, Ukranian, Japanese, Norwegian) in orderings
-          for (dog, snails, fox, horse, ZEBRA) in orderings
-          for (coffee, tea, milk, oj, WATER) in orderings
-          for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
           if Englishman is red            #2
+          if imright(green, ivory)        #6
+          if Norwegian is first           #10
+          if nextto(Norwegian, blue)       #15
+   
+          for (dog, snails, fox, horse, ZEBRA) in orderings
           if Spaniard is dog              #3
+
+          for (coffee, tea, milk, oj, WATER) in orderings
           if coffee is green              #4
           if Ukranian is tea              #5
-          if imright(green, ivory)        #6
-          if OldGold is snails            #7
-          if Kols is yellow               #8
           if milk is middle               #9
-          if Norwegian is first           #10
+          
+          for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
+          if OldGold is snails            #7
+          if Kools is yellow               #8
           if nextto(Chesterfields, fox)   #11
           if nextto(Kools, horse)         #12
           if LuckyStrike is oj            #13
           if Japanese is Parliaments      #14
-          if nexto(Norwegian, blue)       #15
           )
+
+print zebra_puzzle()
