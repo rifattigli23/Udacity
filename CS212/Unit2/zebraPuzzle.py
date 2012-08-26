8240a888714ce01475fefb7627e9b777d022a201
Graham Madden <maddenwg@gmail.com>
2012-08-26 16:20:08 -0400

    optimized timedcalls, added ability to run for n times or seconds

--- CS212/Unit2/zebraPuzzle.py
+++ CS212/Unit2/zebraPuzzle.py
@@ -36,4 +36,29 @@ def zebra_puzzle():
           if Japanese is Parliaments      #14
           )
 
-print zebra_puzzle()
+import time
+
+def timedcall(fn, *args):
+    "Call function with args; return the time in seconds and result."
+    t0 = time.clock()
+    result = fn(*args)
+    t1 = time.clock()
+    return t1-t0, result
+
+def average(numbers):
+    "Return the average (arithmetic mean) of a sequence of numbers."
+    return sum(numbers) / float(len(numbers)) 
+
+def timedcalls(n, fn, *args):
+    """Call fn(*args) repeatedly: n times if n is an int, or up to
+    n seconds if n is a float; return the min, avg, and max time"""
+    # Your code here.
+    if isinstance(n, int):
+      times = [timedcall(fn, *args)[0] for _ in range(n)]
+    else:
+      times = []
+      while sum(times) < n:
+        times.append(timedcall(fn, *args)[0])
+    return min(times), average(times), max(times)
+
+print timedcalls(10.0, zebra_puzzle, )
