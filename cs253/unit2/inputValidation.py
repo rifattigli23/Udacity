e4655aea809975de5199c96a2fe22164c1545055
Graham Madden <maddenwg@gmail.com>
2012-10-13 14:03:38 -0400

    add year validation

--- cs253/unit2/inputValidation.py
+++ cs253/unit2/inputValidation.py
@@ -72,5 +72,27 @@ def valid_day(day):
 # valid_day('15') => 15
 # valid_day('500') => None
 
+# -----------
+# User Instructions
+# 
+# Modify the valid_year() function to verify 
+# whether the string a user enters is a valid 
+# year. If the passed in parameter 'year' 
+# is not a valid year, return None. 
+# If 'year' is a valid year, then return 
+# the year as a number. Assume a year 
+# is valid if it is a number between 1900 and 
+# 2020.
+#
 
+def valid_year(year):
+   if year and year.isdigit():
+        year = int(year)
+        if year >= 1900 and year <= 2020:
+            return year
+
+# valid_year('0') => None    
+# valid_year('-11') => None
+# valid_year('1950') => 1950
+# valid_year('2000') => 2000
 
