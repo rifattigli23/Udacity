713c216f5ea26aff6c3fdab3ec82a38f75ba01c4
Graham Madden <maddenwg@gmail.com>
2012-10-12 23:48:25 -0400

    simplified to check first three letters of input

--- cs253/unit2/inputValidation.py
+++ cs253/unit2/inputValidation.py
@@ -22,8 +22,14 @@ months = ['January',
           'October',
           'November',
           'December']
+
+month_abbvs = dict((m[:3].lower(),m) for m in months)        
           
 def valid_month(month):
+    if month:
+        short_month = month[:3].lower()
+        return month_abbvs.get(short_month)
+        
     for m in months:
         if month.upper() == m.upper(): return m
     return None
