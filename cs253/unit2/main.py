dda6a52c8f5c279a2890ce3c005a06d730b94379
Graham Madden <maddenwg@gmail.com>
2012-10-12 23:22:21 -0400

    Add three text inputs for month,day,year

--- cs253/unit2/main.py
+++ cs253/unit2/main.py
@@ -17,14 +17,20 @@
 import webapp2
 
 form="""
-<form method="post" action="/testform">
-    <input name="q">
+<form method="post">
+    What is your birthday?
+    <br>
+    <input type="text" name="month">
+    <input type="text" name="day">
+    <input type="text" name="year">
+
+    <br>
+    <br>
     <input type="submit">
 </form>
 """
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        #self.response.headers['Content-Type'] = 'text/html'
         self.response.out.write(form)
         
 app = webapp2.WSGIApplication([('/', MainHandler),
