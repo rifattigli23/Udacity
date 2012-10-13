94b19db0ea20d130c82546b71343f19febba1f9b
Graham Madden <maddenwg@gmail.com>
2012-10-13 14:01:18 -0400

    simplified with professors version

--- cs253/unit2/inputValidation.py
+++ cs253/unit2/inputValidation.py
@@ -40,10 +40,6 @@ def valid_month(month):
 # valid_month("foo") => None
 # valid_month("") => None
 
-print valid_month("january")   
-print valid_month("January")
-print valid_month("foo")
-print valid_month("")
 
 # -----------
 # User Instructions
@@ -66,23 +62,15 @@ print valid_month("")
 #
 
 def valid_day(day):
-    try:
-        day_int = int(day)
-    except TypeError: 
-        return None
-    if day_int >= 1 and day_int <= 31:
-        return day_int
-    else:
-        return None
-        
+    if day and day.isdigit():
+        day = int(day)
+        if day > 0 and day <= 31:
+            return day
+            
 # valid_day('0') => None    
 # valid_day('1') => 1
 # valid_day('15') => 15
 # valid_day('500') => None
 
-print valid_day('0')
-print valid_day('1')
-print valid_day('15')
-print valid_day('500')
 
 
