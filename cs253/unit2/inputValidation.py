98bb1aef170d32ed4b841bd931eee637345c9390
Graham Madden <maddenwg@gmail.com>
2012-10-13 13:58:05 -0400

    add day validation

--- cs253/unit2/inputValidation.py
+++ cs253/unit2/inputValidation.py
@@ -45,3 +45,44 @@ print valid_month("January")
 print valid_month("foo")
 print valid_month("")
 
+# -----------
+# User Instructions
+# 
+# Modify the valid_day() function to verify 
+# whether the string a user enters is a valid 
+# day. The valid_day() function takes as 
+# input a String, and returns either a valid 
+# Int or None. If the passed in String is 
+# not a valid day, return None. 
+# If it is a valid day, then return 
+# the day as an Int, not a String. Don't 
+# worry about months of different length. 
+# Assume a day is valid if it is a number 
+# between 1 and 31.
+# Be careful, the input can be any string 
+# at all, you don't have any guarantees 
+# that the user will input a sensible 
+# day.
+#
+
+def valid_day(day):
+    try:
+        day_int = int(day)
+    except TypeError: 
+        return None
+    if day_int >= 1 and day_int <= 31:
+        return day_int
+    else:
+        return None
+        
+# valid_day('0') => None    
+# valid_day('1') => 1
+# valid_day('15') => 15
+# valid_day('500') => None
+
+print valid_day('0')
+print valid_day('1')
+print valid_day('15')
+print valid_day('500')
+
+
