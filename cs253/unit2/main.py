af5b5b83ce9696262be454ae9ee9d905548061e1
Graham Madden <maddenwg@gmail.com>
2012-10-13 13:57:25 -0400

    add labels to the text inputs

--- cs253/unit2/main.py
+++ cs253/unit2/main.py
@@ -20,18 +20,31 @@ form="""
 <form method="post">
     What is your birthday?
     <br>
-    <input type="text" name="month">
-    <input type="text" name="day">
-    <input type="text" name="year">
+    
+    <label> Month
+        <input type="text" name="month">
+    </label>
+
+    <label> Day
+        <input type="text" name="day">
+    </label>
+
+    <label> Year
+        <input type="text" name="year">
+    </label>
 
     <br>
     <br>
     <input type="submit">
 </form>
 """
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.out.write(form)
+    
+    def post(self):
+        self.response.out.write("Thanks! That's a totally valid day!")
         
 app = webapp2.WSGIApplication([('/', MainHandler),
 ], debug=True)
