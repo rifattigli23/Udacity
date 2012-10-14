2d15f46892bd093e3dba3287aaf8e50d29b8e360
Graham Madden <maddenwg@gmail.com>
2012-10-14 17:55:44 -0400

    update to support templates, multiple apps, smarter design per Udacity prof

--- cs253/unit2/rot13/main.py
+++ cs253/unit2/rot13/main.py
@@ -1,74 +1,94 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
+import re
+from string import letters
+
 import webapp2
-from inputValidation import valid_day, valid_month, valid_year
-from html_util import escape_html
-import string
+import jinja2
 
-header="""
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Unit 2 Rot 13</title>
-  </head>
-  <body>
-    <h2>Enter some text to ROT13:</h2>
-"""
+from google.appengine.ext import db
 
-form="""
-<form method="post">
-    <textarea name="text" 
-              style="height: 100px; width: 400px;">%(text)s</textarea>
-    <br>
-    <input type="submit">
-</form>
-"""
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
 
-footer="""
-  </body>
-</html>
-"""
-
-class MainHandler(webapp2.RequestHandler):
-    def rot13(self, text):
-        upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        lower="abcdefghijklmnopqrstuvwxyz"
-        text = list(text)
-        for idx, val in enumerate(text):
-            lowerIndex = lower.find(val)
-            upperIndex = upper.find(val)
-            if lowerIndex <> -1:
-                text[idx] = lower[(lowerIndex+13)%26]
-            elif upperIndex <> -1:
-                text[idx] = upper[(upperIndex+13)%26]
-                     
-        return "".join(text)
-        
-    def write_form(self, text=""):
-        self.response.out.write(header)
-        self.response.out.write(form % {"text": escape_html(text)})
-        self.response.out.write(footer)
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
     
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
+        
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+        
+class Rot13(BaseHandler):
     def get(self):
-        self.write_form()
-    
-    def post(self):        
-        text = self.request.get('text')
-        text = self.rot13(text)
-        self.write_form(text)
+        self.render('rot13-form.html')
         
-app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+    def post(self):
+        rot13 = ''
+        text = self.request.get('text')
+        if text:
+            rot13 = text.encode('rot13')
+        
+        self.render('rot13-form.html', text = rot13)
+        
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+    
+class Signup(BaseHandler):
+
+    def get(self):
+        self.render("signup-form.html")
+    
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+            
+        params = dict(username = username, 
+                      email = email)
+        if not valid_username(username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+    
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+    
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+    
+        if have_error:
+            self.render('signup-form.html', **params)
+        else:
+            self.redirect('/unit2/selcome?username=' + username)
+
+class Welcome(BaseHandler):
+    def get(self):
+        username = self.request.get('username')
+        if valid_username(username):
+            self.render('welcome.html', username = username)
+        else:
+            self.redirect('/unit2/signup')
+            
+app = webapp2.WSGIApplication([('/unit2/rot13', Rot13),
+                               ('/unit2/signup', Signup),
+                               ('/unit2/welcome', Welcome)],
+                               debug=True)
