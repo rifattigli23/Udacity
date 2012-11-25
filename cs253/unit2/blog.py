2b4a8e1c6d084089bd06fd37e10c5df2961481a4
Graham Madden <maddenwg@gmail.com>
2012-11-25 09:54:53 -0500

    added last_modified to Post object model, replaced \n with <br> for html linebreaks.

--- cs253/unit2/blog.py
+++ cs253/unit2/blog.py
@@ -26,6 +26,11 @@ class Post(db.Model):
     subject = db.StringProperty(required = True)
     content = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("blog.html", p = self)
 
 # Render all posts
 class BlogHandler(BaseHandler):
