541a6b350a711a602dd0c73a6abb917c5765c1d0
Graham Madden <maddenwg@gmail.com>
2013-01-02 14:16:37 -0500

    corrected class methods so they reference cls and not User

--- cs253/blog/blog.py
+++ cs253/blog/blog.py
@@ -93,17 +93,17 @@ class User(db.Model):
     
     @classmethod
     def by_id(cls, uid):
-        return User.get_by_id(uid, parent = users_key())
+        return cls.get_by_id(uid, parent = users_key())
     
     @classmethod
     def by_name(cls, name):
-        u = User.all().filter('name =', name).get()
+        u = cls.all().filter('name =', name).get()
         return u
 
     @classmethod
     def register(cls, name, pw, email = None):
         pw_hash = make_pw_hash(name, pw)
-        return User(parent = users_key(),
+        return cls(parent = users_key(),
                     name = name,
                     pw_hash = pw_hash,
                     email = email)
