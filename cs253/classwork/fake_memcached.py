2cdc342551a2a901dedaf5eaa0a1009282dc2fca
Graham Madden <maddenwg@gmail.com>
2013-01-04 16:12:34 -0500

    official answer

--- cs253/classwork/fake_memcached.py
+++ cs253/classwork/fake_memcached.py
@@ -5,47 +5,41 @@ CACHE = {}
 
 #return True after setting the data
 def set(key, value):
-    ###Your set code here.
     CACHE[key] = value
-    if CACHE[key]:
-        return True
-    else: 
-        return False
+    return True
 
 #return the value for key
 def get(key):
-    ###Your get code here.
     return CACHE.get(key, None)
 
 #delete key from the cache
 def delete(key):
-    ###Your delete code here.
-    CACHE.pop(key)
+    if key in CACHE:
+        del CACHE[key]
 
 #clear the entire cache
 def flush():
-    ###Your flush code here.
     CACHE.clear()
 
-print set('x', 1)
-#>>> True
-
-print get('x')
-#>>> 1
-
-print get('y')
-#>>> None
-
-delete('x')
-print get('x')
-#>>> None
-
-print set('x', 1)
-#>>> True
-
-print get('x')
-#>>> 1
-
-flush()
-print get('x')
-#>>> None
+# print set('x', 1)
+# #>>> True
+# 
+# print get('x')
+# #>>> 1
+# 
+# print get('y')
+# #>>> None
+# 
+# delete('x')
+# print get('x')
+# #>>> None
+# 
+# print set('x', 1)
+# #>>> True
+# 
+# print get('x')
+# #>>> 1
+# 
+# flush()
+# print get('x')
+# #>>> None
