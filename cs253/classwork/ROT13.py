d2130395bf390a21326fe1d68723d2d5a7663ca5
Graham Madden <maddenwg@gmail.com>
2013-01-06 13:53:56 -0500

    add ROT13 to classwork

---
+++ cs253/classwork/ROT13.py
@@ -0,0 +1,11 @@
+class Rot13(BlogHandler):
+    def get(self):
+        self.render('rot13-form.html')
+    
+    def post(self):
+        rot13 = ''
+        text = self.request.get('text')
+        if text:
+            rot13 = text.encode('rot13')
+        
+        self.render('rot13-form.html', text = rot13)
