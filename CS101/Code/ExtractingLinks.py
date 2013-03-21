3e42aaf8dc8339e06447de75e654607c36f0298b
Graham Madden <maddenwg@gmail.com>
2013-03-21 06:54:03 -0400

    improve variable names

--- CS101/Code/ExtractingLinks.py
+++ CS101/Code/ExtractingLinks.py
@@ -7,7 +7,7 @@
 page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://udacity.com">')
 start_link = page.find('<a href=')
-start_linkstring = page.find('"', start_link) + 1
-end_linkstring = page.find('"', start_linkstring + 1)
+start_quote = page.find('"', start_link)
+end_quote = page.find('"', start_quote + 1)
 
-print page[start_linkstring:end_linkstring]
+print page[start_quote+1:end_quote]
