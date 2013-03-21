8ce0d287321844e6fedd8d13e71db0a990f19719
Graham Madden <maddenwg@gmail.com>
2013-03-21 06:51:14 -0400

    find first URL on page

--- CS101/Code/ExtractingLinks.py
+++ CS101/Code/ExtractingLinks.py
@@ -1,12 +1,13 @@
-# Write Python code that initializes the variable
-# start_link to be the value of the position
-# where the first '<a href=' occurs in a page.
+# Write Python code that assigns to the 
+# variable url a string that is the value 
+# of the first URL that appears in a link 
+# tag in the string page.
 
-page = '''<div id="top_bin"> <div id="top_content" class="width960">
-   <div class="udacity float-left"> <a href="/">'''
-
+# page = contents of a web page
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
 start_link = page.find('<a href=')
+start_linkstring = page.find('"', start_link) + 1
+end_linkstring = page.find('"', start_linkstring + 1)
 
-# tests
-print start_link
-print page[95:]
+print page[start_linkstring:end_linkstring]
