7f61a27dafb6ff8b80d6534a2db3af9d3682dd01
Graham Madden <maddenwg@gmail.com>
2013-03-25 22:38:24 -0400

    added class answer as check_sudoku_answer()

--- CS101/Code/Sudoku.py
+++ CS101/Code/Sudoku.py
@@ -78,6 +78,27 @@ def check_sudoku(l):
             if element < 1 or element > len(l[0]): result = False
             
     return result
+    
+def check_sudoku_answer(p):
+    n = len(p) #Extract size of grid
+    digit = 1 #start with 1
+    while digit <= n: #Go through each digit
+        i = 0
+        while i < n: # Go through each row and column
+            row_count = 0
+            col_count = 0
+            j = 0
+            while j < n: #for each entry in ith row/column
+                if p[i][j] == digit: #check row count
+                    row_count = row_count + 1
+                if p[j][i] == digit:
+                    col_count = col_count + 1
+                j = j + 1
+            if row_count != 1 or col_count != 1:
+                return False
+            i = i + 1 #next row/column
+            digit = digit + 1 #next digit
+        return True #Nothing was wrong
 
 # print check_sudoku(incorrect)
 #>>> False
