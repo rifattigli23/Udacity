8792191fbae48e4dfca71943af41ac8317ebb39e
Graham Madden <maddenwg@gmail.com>
2013-03-29 10:29:40 -0400

    add index logic and organize a bit

--- CS101/Code/ExtractingLinks.py
+++ CS101/Code/ExtractingLinks.py
@@ -1,6 +1,37 @@
 ## Udacity CS101 Web Crawler
 import urllib2
+    
+##
+#   Utilities
+##    
+def union(a,b):
+    return list(set(a+b))
+    
+##
+#   Index Stuff
+##    
+def lookup(index,keyword):
+    for entry in index:
+        if (entry[0] == keyword):
+            return entry[1]
+    return []
 
+def add_to_index(index,keyword,url):
+    for entry in index:
+        if entry[0] == keyword:
+            entry[1].append(url)
+            return
+    index.append([keyword,[url]])
+
+def add_page_to_index(index,url,content):
+    contentList = content.split()
+    
+    for keyword in contentList:
+        add_to_index(index, keyword, url)
+
+##
+#   Crawl Stuff
+##
 def get_next_target(page):
     start_link = page.find('<a href=')
     if start_link == -1:
@@ -27,19 +58,21 @@ def get_page(url):
     html = usock.read()
     usock.close()
     return html
-
+    
 def crawl(seed):
     tocrawl = [seed]
     crawled = []
+    index = []
     
     while tocrawl:
         next_crawl = tocrawl.pop()
         if next_crawl not in crawled:
             page = get_page(next_crawl)
+            add_page_to_index(index,next_crawl,page)
             page_links = get_all_links(page)
             union(tocrawl, page_links)
         crawled.append(next_crawl)
-    return crawled
+    return index
 
 seed = 'http://www.udacity.com/cs101x/index.html'
 
