f6a92071074d723732764ba6c6790bdea6c21b24
Graham Madden <maddenwg@gmail.com>
2013-03-30 17:21:12 -0400

    refactor hashtable_update() and hashtable_lookup() to use bucket_find(). Removes duplication of the for loop on buckets

--- CS101/Code/ExtractingLinks.py
+++ CS101/Code/ExtractingLinks.py
@@ -35,23 +35,31 @@ def hashtable_get_bucket(htable,keyword):
 def hashtable_add_entry(htable, key, value):
     bucket = hashtable_get_bucket(htable, key)
     bucket.append([key, value])
+
+def bucket_find(bucket, key):
+    for entry in bucket:
+        if entry[0] == key:
+            return entry
+        else:
+            return None
     
 def hashtable_lookup(htable, key):
     bucket = hashtable_get_bucket(htable, key)
-    for element in bucket:
-        if element[0] == key:
-            return element[1]
+    entry = bucket_find(bucket, key)
+    if entry[0] == key:
+        return entry[1]
     return None
 
 def hashtable_update(htable, key, value):
-    if hashtable_lookup(htable, key):
-        bucket = hashtable_get_bucket(htable, key)
-        for element in bucket:
-            if element[0] == key:
-                element[1] = value
+    bucket = hashtable_get_bucket(htable, key)
+    entry = bucket_find(bucket, key)
+    if entry:
+        entry[1] = value
     else:
-        hashtable_add_entry(htable, key, value)
-    return htable
+        bucket.append([key, value])
+
+
+
 
 ##
 #   Index Stuff
