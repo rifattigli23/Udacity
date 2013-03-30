08628322eab20c9617469a344077e4213c96fe45
Graham Madden <maddenwg@gmail.com>
2013-03-30 10:19:35 -0400

    add string hash function

--- CS101/Code/ExtractingLinks.py
+++ CS101/Code/ExtractingLinks.py
@@ -13,6 +13,11 @@ def split_string(source,splitlist):
         source = source.replace(sep, default_sep)
     return [i.strip() for i in source.split(default_sep) if i != '']
 
+def hash_string(keyword,buckets):
+    sum = 0
+    for c in keyword:
+        sum += ord(c)
+    return sum % buckets  
     
 ##
 #   Index Stuff
@@ -97,6 +102,51 @@ def crawl(seed):
         crawled.append(next_crawl)
     return index
 
+
+##
+# TESTING
+##
+
+# execution timer
+import time
+
+def time_execution(code):
+    start = time.clock()
+    result = eval(code)
+    run_time = time.clock() - start
+    print result, run_time
+    
+def spin_loop(n):
+    i = 0
+    while i < n:
+        i = i + 1
+    return i
+
+# make big indexes
+def make_string(p):
+    s = ""
+    for e in p:
+        s = s + e
+    return s
+
+def make_big_index(size):
+    index = []
+    letters = ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']
+    while len(index) < size:
+        word = make_string(letters)
+        add_to_index(index, word, 'fake')
+        for i in range(len(letters) - 1, 0, -1):
+            if letters[i] < 'z':
+                letters[i] = chr(ord(letters[i]) +1)
+                break
+            else:
+                letters[i] = 'a'
+    return index
+
 seed = 'http://www.udacity.com/cs101x/index.html'
 
-print crawl(seed)
+# print crawl(seed)
+
+index10000 = make_big_index(10000)
+time_execution('lookup(index10000, "udacity")')
+
